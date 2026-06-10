73da78c57897a64cd3e5d4d9418f6fa39324a693
Sharva <motegaonkarsharva@gmail.com>
2026-06-11 03:54:22 +0530

    Add files via upload

---
+++ introduction.java
@@ -0,0 +1,36 @@
+public class introduction {
+    public static void main(String args[]){
+        System.out.println("hello java");
+    }
+}
+/*
+**1)how is my code running?
+->JDK(java development kit)
+    -> +development tools
+    ->JRE(java runtime enviroment)
+        -> +libraries
+        ->JVM
+
+1)compilation
+->the code we writen in java is source code   
+-> the source code goes to complier
+-> and turns it into byte code (.class) 
+->this byte code can perform on any  system windows,linus,etc
+->ts cannot happen in c++ thats why java is called portable language
+
+2)execution
+->takes the byte code and converts it into native code(0 and 1)
+
+**2) class and function
+ 1)function
+ ->there are many functions in a class
+ void.main{
+ }
+ 2)class
+ class main{
+    void main(){
+    }
+}
+ */
+
+
